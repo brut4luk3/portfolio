63068765f8939ca973aa9393d711e10c4bb919b7
brut4luk3 <lucasreinert96@gmail.com>
2023-10-02 17:16:15 -0300

    Inclusão api telefone latam

--- api_cpf_cnpj/views.py
+++ api_cpf_cnpj/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
 import requests
-from datetime import datetime
 import json
 
 def index_api_cpf_cnpj(request):
     if request.method == 'GET':
 
-        return render(request, 'api_compara_datas.html')
+        return render(request, 'api_cpf_cnpj.html')
 
     elif request.method == 'POST':
 
-        post_data = request.POST.copy()
-        data1 = datetime.strptime(post_data['data1'], '%Y-%m-%d').strftime('%d/%m/%Y')
-        data2 = datetime.strptime(post_data['data2'], '%Y-%m-%d').strftime('%d/%m/%Y')
-        modo = post_data.get('modo')
+        post_data = request.POST
 
-        post_data['data1'] = data1
-        post_data['data2'] = data2
-
-        response = requests.post('http://brut4luk3.pythonanywhere.com/api/compara_datas', json=post_data)
+        response = requests.post('https://api-valida-cpf-cnpj.onrender.com/api/valida_cpf_cnpj', json=post_data)
 
         try:
             response_json = response.json()
@@ -31,23 +24,14 @@ def index_api_cpf_cnpj(request):
 
             resultado = response_json.get('result')
 
-            if modo == 'comparacao':
-                if resultado == '1':
-                    resultado_formatado = 'Data 1 é maior que Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+            if resultado == True:
 
-                elif resultado == '-1':
-                    resultado_formatado = 'Data 1 é menor que Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+                resultado_descricao = 'Este documento está em um formato válido.'
 
-                elif resultado == '0':
-                    resultado_formatado = 'Data 1 é igual a Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
-
-            else:
-                resultado_formatado = resultado
-                return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+                return render(request, 'api_cpf_cnpj.html', {'resultado_descricao': resultado_descricao})
 
         else:
-            resultado_formatado = 'Houve um erro na API!'
-            return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+
+            resultado_descricao = 'Este documento está em um formato inválido.'
+
+            return render(request, 'api_cpf_cnpj.html', {'resultado_descricao': resultado_descricao})
--- api_telefone_latam/views.py
+++ api_telefone_latam/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
 import requests
-from datetime import datetime
 import json
 
 def index_api_telefone_latam(request):
     if request.method == 'GET':
 
-        return render(request, 'api_compara_datas.html')
+        return render(request, 'api_telefone_latam.html')
 
     elif request.method == 'POST':
 
-        post_data = request.POST.copy()
-        data1 = datetime.strptime(post_data['data1'], '%Y-%m-%d').strftime('%d/%m/%Y')
-        data2 = datetime.strptime(post_data['data2'], '%Y-%m-%d').strftime('%d/%m/%Y')
-        modo = post_data.get('modo')
+        post_data = request.POST
 
-        post_data['data1'] = data1
-        post_data['data2'] = data2
-
-        response = requests.post('http://brut4luk3.pythonanywhere.com/api/compara_datas', json=post_data)
+        response = requests.post('https://api-valida-telefone-latam.onrender.com/api/valida_telefone_latam', json=post_data)
 
         try:
             response_json = response.json()
@@ -29,25 +22,24 @@ def index_api_telefone_latam(request):
 
         if response.status_code == 200 and response_json is not None:
 
-            resultado = response_json.get('result')
+            regiao = response_json.get('regiao')
+            valid = response_json.get('valid')
 
-            if modo == 'comparacao':
-                if resultado == '1':
-                    resultado_formatado = 'Data 1 é maior que Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+            context = {
+                'regiao': regiao,
+                'valid': valid
+            }
 
-                elif resultado == '-1':
-                    resultado_formatado = 'Data 1 é menor que Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
-
-                elif resultado == '0':
-                    resultado_formatado = 'Data 1 é igual a Data 2'
-                    return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
-
-            else:
-                resultado_formatado = resultado
-                return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+            return render(request, 'api_telefone_latam.html', context=context)
 
         else:
-            resultado_formatado = 'Houve um erro na API!'
-            return render(request, 'api_compara_datas.html', {'resultado_formatado': resultado_formatado})
+
+            regiao = response_json.get('regiao')
+            valid = response_json.get('valid')
+
+            context = {
+                'regiao': regiao,
+                'valid': valid
+            }
+
+            return render(request, 'api_telefone_latam.html', context=context)
